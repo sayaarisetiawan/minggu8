bd4ab2145daa0ebecd18cbc98566ee24550bbc25
sayaarisetiawan <ari_22510010@stimata.ac.id>
2023-03-02 13:26:26 +0700

    latihan 1

---
+++ latihan1.php
@@ -0,0 +1,17 @@
+<?php 
+//penulisan array
+$hari = array ("senin", "selasa", "rabu");
+$bulan = ["januari", "februari", "maret"];
+$gabungan = [100, "teks", true];
+
+//memanggil array
+//versi debugging atau versi programmer
+var_dump($hari);
+echo "<br>";
+print_r($bulan);
+echo "<br>";
+
+//versi user
+
+echo $gabungan[0];
+?>
