e8b3b0b3c26b07dba6e9fa46d8c547f695c27a59
sayaarisetiawan <ari_22510010@stimata.ac.id>
2023-03-17 16:36:06 +0700

    metode POST

---
+++ minggu9/latihan3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Metode POST</title>
+</head>
+<body>
+    <!-- metode get untuk menampilkan url key value
+    metode post untuk menyembunyikan input key value -->
+    <form action="latihan4.php" method="POST">
+    Masukkan nama:
+    <input type="text" name="nama">
+    <br>
+    <button type="submit" name="submit">
+    Kirim
+    </button>
+    </form>
+</body>
+</html>
